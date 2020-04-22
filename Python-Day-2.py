15a080d65d778a759503cccff9fa6fbab70c96ec
Richard Peterson <richardpeterson@Richards-MacBook-Pro.local>
2020-04-21 20:57:55 -0500

    simple message exerceise

--- Python-Day-2.py
+++ Python-Day-2.py
@@ -1,1 +1,10 @@
 # Python CC -  Variables and Simple Data Types
+
+message = "Hello :)"
+print(message)
+
+message = "Hello Python CC World :O"
+print(message)
+
+# print(mesage) - typical typo error
+
