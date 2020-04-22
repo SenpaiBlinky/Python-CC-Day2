a8ed01821fce57203b9b25476a046402157bb595
Richard Peterson <richardpeterson@Richards-MacBook-Pro.local>
2020-04-21 21:35:28 -0500

    covered strings and methods as well as removing and adding white space

--- Python-Day-2.py
+++ Python-Day-2.py
@@ -8,3 +8,26 @@ print(message)
 
 # print(mesage) - typical typo error
 
+# --------------------------------------------------------Strings and Methods-ish-----------------------------------------------------------
+
+
+name = "richard peterson"
+print(name.title())  # makes the string title case
+print(name.upper())  # makes the string upper case
+print(name.lower())  # makes the string lowe case
+
+print("here is the name uppercase" + " " + name.upper() + " " + "and here is the name lowercase " + name.lower() + " using concatenation :)")
+
+
+# ---------------------white space adding with tabs or newlines---------------------
+
+print("\tLanguages:\nPython\nJavaScript\nC#\nHTML\nCSS")
+
+# ---------------------removing white space using strip-----------------------------
+
+language = " Python "
+no_white_space_on_right = language.rstrip()
+
+print(no_white_space_on_right) # however this removes only right (left would be with an "l" infront)
+
+print(no_white_space_on_right.strip()) # removes both left and right (not in between)
